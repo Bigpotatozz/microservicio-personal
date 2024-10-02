900536bd115133c1313212d95da97484ad195841
oscar <oscarmancilla227@gmail.com>
2024-10-02 12:22:29 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Permite todos los orígenes
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: '*', // Permite todos los headers
+  });
   await app.listen(3000);
 }
 bootstrap();
